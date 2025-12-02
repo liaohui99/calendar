42350c4ae7fb4a3e52db9db1269ba751bf1355d6
liaohui <liaohui@lundong.com.cn>
2025-12-02 14:53:56 +0800

    初步完成对话页面

---
+++ backend/src/main/java/com/calendar/chart/ai/req/PromptReq.java
@@ -0,0 +1,15 @@
+package com.calendar.chart.ai.req;
+
+import dev.langchain4j.service.UserMessage;
+
+/**
+ * @author Gabriel
+ * @version 1.0
+ * @date 2025/12/2 11:28
+ * @description: TODO
+ */
+public class PromtReq {
+    int memoryId, @UserMessage
+    String userMessage
+
+}
