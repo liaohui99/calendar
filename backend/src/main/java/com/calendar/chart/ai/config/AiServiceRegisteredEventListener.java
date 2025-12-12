26cffc53f0b184ba5d22721867be749a9a1a50c5
liaohui <liaohui@lundong.com.cn>
2025-12-12 14:23:19 +0800

    优化对话

--- backend/src/main/java/com/calendar/chart/ai/config/AiServiceRegisteredEventListener.java
+++ backend/src/main/java/com/calendar/chart/ai/config/AiServiceRegisteredEventListener.java
@@ -14,7 +14,7 @@ class AiServiceRegisteredEventListener implements ApplicationListener<AiServiceR
     @Override
     public void onApplicationEvent(AiServiceRegisteredEvent event) {
         Class<?> aiServiceClass = event.aiServiceClass();
-        event.toolSpecifications()
+        List<ToolSpecification> toolSpecifications = event.toolSpecifications();
         for (int i = 0; i < toolSpecifications.size(); i++) {
             System.out.printf("[%s]: [Tool-%s]: %s%n", aiServiceClass.getSimpleName(), i + 1, toolSpecifications.get(i));
         }
